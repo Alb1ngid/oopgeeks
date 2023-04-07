d20821c66d85181a2254e88961d37fc5b2f0eba9
bekkkks <ewefe811@gmail.com>
2023-04-07 17:04:21 +0600

    lesson2

---
+++ lessons/lesson2.py
@@ -0,0 +1,43 @@
+# Парадигмы ооп
+# наследование полиморфизм
+
+class Human:  # супер класс
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def run(self):
+        print(f'{self.name} runs')
+
+
+class Student(Human):  # дочерний класс
+    def run(self):
+        print(f'{self.name} fly')
+
+    def fale(self):
+        print('fale')
+
+
+class Batrak(Student):
+    def __init__(self, name, age, last):
+        # Human.__init__(self,name,age)
+        super().__init__(name, age)
+        self.last = last
+
+    def new(self):
+        Human.run(self)
+        super().run()
+
+
+c = Batrak('адахан', 18, 'last')
+c.new()
+
+human = Human('beka', 19)
+student = Student('станислав', 11)
+student.run()
+human.run()
+
+print(Batrak.mro())
+
+print(object)
+
--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-# This is a sample Python script.
+class A:
+    a = 1
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    def a1(self):
+        print(self.a)
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def __str__(self):
+        return f'{self.name} {self.age}'
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+a = A('name', 11)
+print(a)
